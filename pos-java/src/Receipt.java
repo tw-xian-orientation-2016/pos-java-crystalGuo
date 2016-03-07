4682c98b7f943cf6a3949fa314460e7b9ef08ed8
crystal <793832732@qq.com>
2016-03-07 22:57:19 +0800

    generate receipt

--- pos-java/src/Receipt.java
+++ pos-java/src/Receipt.java
@@ -32,6 +32,40 @@ public class Receipt {
         return finalSaved;
     }
 
-    
+    public String getReceipt() {
+
+        String receipt = "***<没钱赚商店>收据***\n";
+
+        for (ReceiptItem receiptItem: receiptItems) {
+            receipt +=
+                    "名称：" + receiptItem.getName() + "，" +
+                    "数量：" + formatCount(receiptItem.getCount()) + receiptItem.getUnit() + "，" +
+                    "单价：" + formatPrice(receiptItem.getPrice()) + "(元)，" +
+                    "小计：" + formatPrice(receiptItem.getTotalPrice()) + "(元)\n";
+        }
+
+        receipt += "----------------------\n";
+        receipt +=
+                "总计：" + formatPrice(getFinalPrice()) + "(元)\n" +
+                "节省：" + formatPrice(getFinalSaved()) + "(元)\n" +
+                        "**********************";
+
+        return receipt;
+
+    }
+
+    private String formatPrice(double price) {
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        return decimalFormat.format(price);
+    }
+
+    private String formatCount(double count) {
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.##");
+
+        return decimalFormat.format(count);
+    }
 
 }
