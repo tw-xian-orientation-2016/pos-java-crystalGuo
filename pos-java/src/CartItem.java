9aa83ed8ffa468054b799b6171a1e8ab0e22b40f
crystal <793832732@qq.com>
2016-03-07 22:09:52 +0800

    add getPromotionType method

--- pos-java/src/CartItem.java
+++ pos-java/src/CartItem.java
@@ -2,6 +2,7 @@ public class CartItem {
 
     private Item item;
     private double count;
+    private String promotionType;
 
     public CartItem(String barcode, double count, Item[] allItems) {
         this.item = findItem(barcode, allItems);
@@ -25,5 +26,24 @@ public class CartItem {
 
     public double getCount() {
         return count;
+    }
+
+    public void setPromotionType(String promotionType) {
+        this.promotionType = promotionType;
+    }
+
+    public String getPromotionType(String barcode, Promotion[] promotions) {
+
+        for (int i = 0; i < promotions.length; i++) {
+
+            String[] barcodes = promotions[i].getBarcodes();
+
+            for (int index = 0; index < barcodes.length; index++) {
+                if (barcode.equals(barcodes[index])) {
+                    return promotions[i].getType();
+                }
+            }
+        }
+        return "";
     }
 }
